ac33daa6e8abe0568236a731f6fdfa170a42e89f
khalidsohaibrana <32528388+khalidsohaibrana@users.noreply.github.com>
2023-05-27 21:59:31 +0500

    navbar

--- client/tradex/src/App.js
+++ client/tradex/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-
+import Home from './components/mainpage/Home'
+import Navbar1 from './components/Navbar1'
 function App() {
   return (
     <div className="App">
+      <Navbar1/>
       <Home/>
     </div>
   );
