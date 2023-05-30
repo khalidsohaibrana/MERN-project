b08deff022b65b71c09f089f331ca8f756883e62
asifrana86 <134046698+asifrana86@users.noreply.github.com>
2023-05-30 11:08:03 +0500

    login form

--- client/tradex/src/App.js
+++ client/tradex/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/login-signup/loginform';
+import Signup from './components/login-signup/signup';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <LoginForm/>
+      <Signup/>
       <Home/>
     </div>
   );
