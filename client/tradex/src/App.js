3f329f3a1630fa1e7624994310c0637c856c7962
khalidsohaibrana <32528388+khalidsohaibrana@users.noreply.github.com>
2023-05-30 16:26:39 +0500

    push2

--- client/tradex/src/App.js
+++ client/tradex/src/App.js
@@ -4,11 +4,13 @@ import Home from './components/mainpage/Home'
 import Navbar1 from './components/Navbar1'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import AddListingForm from './components/createNFT/AddListingForm';
 function App() {
   return (
     <div className="App container align-items-center">
       <Navbar1/>
       <Home/>
+      <AddListingForm/>
     </div>
   );
 }
--- client/tradex/src/components/Navbar1.js
+++ client/tradex/src/components/Navbar1.js
@@ -6,6 +6,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import AddListingForm from './createNFT/AddListingForm';
 
 
 function Navbar1() {
@@ -91,7 +92,7 @@ function Navbar1() {
         </Nav>
         <Nav className='ms-auto d-flex align-items-center'>
           <NavbarCollapse className='navbar-nav navbar-right-wrap ms-auto d-flex nav-top-wrap navbar-nav'>
-              <Button onClick={()=>{alert("ADD Nft form")}} >Create NFT</Button>
+              <Button   >Create NFT</Button>
               <span className='d-flex' style={{}}>
               <ul class="navbar-right-wrap ms-2 d-flex nav-top-wrap navbar-nav"  >
                   <a style={{backgroundColor:'lightgray'}} className='text-dark icon-notifications me-lg-1 btn btn-light btn-icon rounded-circle indicator indicator-primary text-muted  '>
